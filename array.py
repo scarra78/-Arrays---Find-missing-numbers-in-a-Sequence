bada3380ef712089a4a9f000541606223855435c
Anthony Scarr <75136691+scarra78@users.noreply.github.com>
2021-08-09 22:17:39 +0100

    Add files via upload
    This will find next even number from a list of postive numbers . To find odd or even there are comments in the script that highlight how it can be done. if you want to include negative numbers and still find the next postive missing number you could do a small for loop that will either create a new list with positive numbers only  or if you want be lazy you could add and z>=0.

---
+++ array.py
@@ -0,0 +1,31 @@
+import random
+
+
+randomlist = []
+
+
+def array():
+    global randomlist
+
+    x = int(input("please enter the number of postive numbers you want to input: ")) #this is to input the ammount of number in an array or numbers 
+
+    for i in range(0, x): #genrates x ammount of numbers 
+        n = random.randint(1,99)
+        if n not in randomlist:
+         randomlist.append(n)
+
+    randomlist.sort()
+    y = max(randomlist)
+    v = min(randomlist)
+    z=v
+    for i in range(v, y):   # finds missing number 
+        if z not in randomlist and z%2==0:  #if you remove everything after and it will find the next missing number odd or even 
+            print(randomlist)
+            print(f"\n the missing positive number is {z} ")
+            break
+        else:
+            z += 1
+
+
+array()
+
